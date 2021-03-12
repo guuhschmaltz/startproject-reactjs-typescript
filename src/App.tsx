b6c0bb6b69b3a89067069701cbfd44336b3916f4
Gustavo <guuhschmaltz@gmail.com>
2021-03-12 12:42:43 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import GlobalStyle from './styles/globals';
+
+
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+        <Routes />
+
+      <GlobalStyle />
+    </Router>
+  );
+};
+
+export default App;
